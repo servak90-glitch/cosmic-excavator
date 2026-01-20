778663a915d436bd182d9878102736d3b8acccb0
Yaroslav <servak90@gmail.com>
2026-01-20 20:40:18 +1100

    feat: Add Capacitor Android platform, including default icons, splash screen, and build configurations.

--- android/app/src/main/java/com/voidpiercer/game/MainActivity.java
+++ android/app/src/main/java/com/voidpiercer/game/MainActivity.java
@@ -1,5 +1,24 @@
 package com.voidpiercer.game;
 
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    WindowInsetsControllerCompat windowInsetsController =
+        WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+    // Configure the behavior of the hidden system bars
+    windowInsetsController.setSystemBarsBehavior(
+        WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    );
+    // Hide both the status bar
+    windowInsetsController.hide(WindowInsetsCompat.Type.statusBars());
+  }
+}
